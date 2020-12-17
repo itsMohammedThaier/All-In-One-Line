6e476e515b126769a6e9dd53a19103d851de17db
itsMohammedThaier <mohamadthaier2@gmail.com>
2020-12-17 13:00:19 +0300

    Added the project

---
+++ All In One Line/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace All_In_One_Line
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ All In One Line/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+namespace All_In_One_Line
+{
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InputedText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string resultProccedText = InputedText.Text;
+            resultProccedText = InputedText.Text.Replace(Environment.NewLine, " ");
+            ResultText.Text = resultProccedText;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(ResultText.Text);
+        }
+
+        private void ResultText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
